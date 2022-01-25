5ab5e79397a1d50a4e49da79571e8576f43cc260
uriloop <orlopez@elpuig.xeill.net>
2022-01-25 19:48:55 +0100

    Initial commit

---
+++ src/test/java/MatrixTest.java
@@ -0,0 +1,40 @@
+package test.java;
+
+import main.java.Matrix;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @ParameterizedTest
+    @CsvSource({"5,5", "10,20", "9384,12384"})
+    void getWidth_OK(int width, int height) {
+        Matrix m = new Matrix(width, height);
+        Assertions.assertEquals(width, m.getWidth());
+    }
+
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void fill() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void found() {
+    }
+
+ /*   @Test
+    void toString() {
+    }*/
+}
